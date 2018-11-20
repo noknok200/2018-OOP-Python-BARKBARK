5c0b6290a0c5e61125e906093ce3c6ab09e4bd65
noknok200 <iampeach@sasa.hs.kr>
2018-11-20 11:24:33 +0900

    add reference

--- ggplottest.py
+++ ggplottest.py
@@ -6,9 +6,14 @@ import matplotlib.pyplot as plot
 ex = 0
 var = 1
 
+ex1 = 1
+var1 = 3
+
 d = {'x': np.random.normal(ex, var, 1000),
-     'y': np.random.normal(ex, var, 1000)}
+     'y': np.random.normal(ex1, var1, 1000)}
 
 df = pd.DataFrame(d)
 df.boxplot(column=['x', 'y'])
 plot.show()
+
+# 참고한 페이지: http://uncoded.tistory.com/11
