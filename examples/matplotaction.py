fedc29d42c65defeacd6dca33a24e40241780e54
noknok200 <iampeach@sasa.hs.kr>
2018-11-20 11:49:15 +0900

    Update matplotaction

--- examples/matplotaction.py
+++ examples/matplotaction.py
@@ -1,8 +1,28 @@
+# 참고: http://jakevdp.github.io/blog/2012/08/18/matplotlib-animation-tutorial/
+
 import numpy as np
-from matplotlib import pyplot as plot
+from matplotlib import pyplot as plt
 from matplotlib import animation as ani
 
 
-fig = plot.figure()
-ax = plot.axes(xlim=(0, 2), ylim=(-2, 2))
-line, _ = ax.plot([], [], lw=2)
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(-50, 50))
+line, = ax.plot([], [], lw=2)
+
+
+def init():
+    line.set_data([], [])
+    return line,
+
+
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.tan(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+
+anim = ani.FuncAnimation(
+    fig, animate, init_func=init, frames=200, interval=20, blit=True)
+
+plt.show()
