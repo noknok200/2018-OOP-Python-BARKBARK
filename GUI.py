40677c1faaa762b9c0cc84769838103ba4c0d607
noah0321 <noah@sasa.hs.kr>
2018-11-20 11:26:50 +0900

    commit

--- GUI.py
+++ GUI.py
@@ -12,7 +12,7 @@ def screen():
     tempo_button = tkinter.Button(window, text="key를 눌러 함수 호출", height=10, width=33) #그냥 버튼 생성, 눌러도 실행 안됨.
     tempo_button.grid(row=1, column=2)
 
-    window.bind("<Key>", func=hamsoo) #<Key>는 키보드를 눌렀을 때라는 의미, func=hamsoo 호출을 window창에 묶기.
+    window.bind("<Key>", func=hamsoo) #<Key>는 키보드를 눌렀을 때라는 의미, func=hamsoo 호출을 window창에 묶기
 
     window.mainloop()
 
