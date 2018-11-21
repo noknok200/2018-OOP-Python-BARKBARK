fc51129ab4ff46ee085bd72c918c5113a21e8b22
noah0321 <noah@sasa.hs.kr>
2018-11-21 14:24:36 +0900

    commit

--- GUI.py
+++ GUI.py
@@ -4,11 +4,6 @@ def hamsoo(self): #self라고 적는 것이 필수적
     print("함수 호출이 됨.")
 
 def screen():
-    window = tkinter.Tk()
-    window.title("Game_Table") #Table
-    window.geometry("1285x673+100+10") #size
-    window.resizable(False, False)
-
     tempo_button = tkinter.Button(window, text="key를 눌러 함수 호출", height=10, width=33) #그냥 버튼 생성, 눌러도 실행 안됨.
     tempo_button.grid(row=1, column=2)
 
@@ -16,4 +11,30 @@ def screen():
 
     window.mainloop()
 
+window = tkinter.Tk()
+window.title("Game_Table") #Table
+window.geometry("1285x673+100+10") #size
+window.resizable(False, False)
+
 screen()
+
+"""
+def draw(event):
+    global x0,y0
+    window.create_line(x0,y0,event.x,event.y)
+
+def down(event):
+    global x0,y0
+    x0,y0 = event.x,event.y
+
+def up(event):
+    global x0,y0
+    if(x0,y0) == (event.x,event.y):
+        window.create_line(x0,y0,x0+1,y0+1)
+
+window.bind("<B1-Motion>",draw)
+window.bind("<Button-1>",down)
+window.bind("<ButtonRelease-1>",up)
+window.pack()
+window.mainloop()
+""" #http://noon.tistory.com/1157
