a76723686533419e24b8e9c65312ddcf9951e228
noknok200 <iampeach@sasa.hs.kr>
2018-11-22 15:05:15 +0900

    Update plot_display.py

--- examples/ggplotaction.py
+++ examples/ggplotaction.py
@@ -4,9 +4,23 @@ from matplotlib import pyplot as plot
 from matplotlib import animation
 
 
-str(ggplot(meat, aes('date', 'beef')) +
-    geom_line(color='black') +
-    scale_x_date(breaks=date_breaks('7 years'), labels='%b %Y') +
-    scale_y_continuous(labels='comma'))
+print(ggplot(meat, aes('date', 'beef')) +
+      geom_line(color='black') +
+      scale_x_date(breaks=date_breaks('7 years'), labels='%b %Y') +
+      scale_y_continuous(labels='comma'))
 
-print()
+fig = plot.figure()
+G = ggplot(meat, aes('wearenumberone'))
+
+
+def init():
+    return G,
+
+
+def animate(i):
+    geom_line(list(range(i)), list(range(i)))
+    return G,
+
+
+anim = animation.FuncAnimation(
+    fig, animate, init_func=init, frames=200, interval=1, blit=True)
--- plot_display.py
+++ plot_display.py
@@ -1,4 +1,17 @@
-from marcap import marcap_utils
-"""
-화면출력파일
-"""
+from marcap import marcap_utils as mu
+from matplotlib import pyplot as plot
+from matplotlib import animation
+
+'''
+fig = plot.figure()
+ax = plot.axes(xlim=(0, 2), ylim=(-200, 200))
+line, = ax.plot([], [], lw=2)
+'''
+
+code = '005930'
+l = mu.marcap_date_range('2018-05-01', '2018-10-31', code)
+ml = list(l['Marcap'])
+
+print(ml)
+
+# def init():
