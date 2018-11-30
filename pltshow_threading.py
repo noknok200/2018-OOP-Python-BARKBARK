d6595d8ab7d168b361978f55dc4805181addb4a5
noah0321 <noah@sasa.hs.kr>
2018-11-30 20:35:26 +0900

    commit

--- pltshow_threading.py
+++ pltshow_threading.py
@@ -1,3 +1,10 @@
+"""
+https://matplotlib.org/examples/event_handling/keypress_demo.html
+반드시 참고하기
+matlib.pyplot에서 띄운 figure에 keypress를 바인드 함+ 키를 누를떄마다 그래프가 변환됨-> 함수 실행 가능
+결론: 이득이득
+"""
+
 from __future__ import print_function
 import sys
 import numpy as np
