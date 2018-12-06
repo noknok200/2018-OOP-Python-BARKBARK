a59192576c9dc258060daeeb9bbd81240336e39c
jwon0615 <jwon0615@sasa.hs.kr>
2018-12-06 14:33:01 +0900

    Update plot_core.py

--- plot_core.py
+++ plot_core.py
@@ -81,24 +81,29 @@ def animate(t):
     if t < len(stock_data) - 100:
         ax1.clear()
         ax1.plot(stock_data[t:t + 100])
-        # ax1.plot(range(t,t+100),stock_data[t+100]*points,color='red') #가장 마지막 가격을 선으로 나타냄
-        '''
-        player가 구매한 경우 
-        '''
-        # if state == '매수대기' :
-        #     if keypress.key_pressed() :
-        #         price_buy = stock_data[t+100] #현재가로 매수
-        #         state = '매도대기' #매도대기 상태로 변경
-        #         print(state)
-        #
-        # elif state == '매도대기' :
-        #     if keypress.key_pressed() :
-        #         price_sell=stock_data[t+100] #매도 대기중에 버튼을 누르면 현재가로 매도
-        #         asset = cal_asset(asset, price_buy, price_sell) #자본 계산
-        #         state = '매수대기' #매수대기 상태로 변경
-        #         print(state)
-        #         print(asset)
-        # ax1.plot(range(t, t + 100), price_buy * points, color='blue')  # 매도대기 상태에서는 현재 얼마에 매수하였는지 표시
+        
+        #현황 출력
+        plt.title(str(asset), loc='left')
+        plt.title(str(round(d_asset*100,2)), loc='right')
+        
+        color_select = selecter(click_time, t+100)
+
+        #매도시 자산 계산
+        if first_click ==0 and click_time !=0 and t+99<=click_time:
+            price_sell = stock_data[t+99]
+            prev_asset=asset
+            
+            
+            #현황 출력
+            prev_asset=asset
+            asset=cal_asset(asset,price_buy,price_sell)
+            d_asset=(asset-prev_asset)/asset
+            print(str(asset)+' '+str(d_asset*100))
+
+        #매수시 구매가격 저장
+        elif first_click==1 and click_time !=0 and t+99<=click_time:
+            price_buy = stock_data[t+99]
+
         if first_click == 1 and click_time != 0:
             color_select = selecter(click_time, t+100)
             clicking_plotter(t, t+100, click_time, t+100, color_select)
@@ -115,8 +120,15 @@ def animate(t):
                 clicking_plotter(
                     t, t+100, opponent_imfo[0], opponent_imfo[1], 'gray')
 
-    else:
-        plt.close(fig=fig)
+    else: #when the game is end,
+        if first_click==1 :
+            asset=cal_asset(asset,price_buy,price_sell)
+            d_asset=(asset-1e8)/asset
+            print(str(asset)+' '+str(d_asset*100))
+        else :
+            d_asset=(asset-1e8)/asset
+            print(str(asset)+' '+str(d_asset*100))
+        plt.pause(100000)
 # for _ in len(player_list) :
     #     ax1.plot(range(i,i+100),player_list[_][0],)
 
